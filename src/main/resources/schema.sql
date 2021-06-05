4922f14dbe598474368f86abb85783469d9025af
Mihir Parekh <mihir.m.parekh@gmail.com>
2021-06-05 11:02:48 -0400

    initial checkin

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Movies (
+    id   INTEGER      NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    actors VARCHAR(128) NOT NULL,
+    release VARCHAR(10) NOT NULL,
+    description VARCHAR(128) NOT NULL,
+    rating INTEGER
+    );
