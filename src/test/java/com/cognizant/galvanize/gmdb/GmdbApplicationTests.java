93ef07e5e29c2ce9f82bc2646f71e83d261954ff
Mihir Parekh <mihir.m.parekh@gmail.com>
2021-06-05 14:39:10 -0400

    get movie if present

--- src/test/java/com/cognizant/galvanize/gmdb/GmdbApplicationTests.java
+++ src/test/java/com/cognizant/galvanize/gmdb/GmdbApplicationTests.java
@@ -1,9 +1,12 @@
 package com.cognizant.galvanize.gmdb;
 
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
+@Data
 class GmdbApplicationTests {
 
 	@Test
--- src/test/java/com/cognizant/galvanize/gmdb/MovieControllerTest.java
+++ src/test/java/com/cognizant/galvanize/gmdb/MovieControllerTest.java
@@ -8,15 +8,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 
 import javax.transaction.Transactional;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -40,9 +38,7 @@ public class MovieControllerTest {
                 .contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuiler).andExpect(status().isOk())
                 .andExpect(content().string("{\"message\":\"no movies found\"}"));
-
-
-    }
+     }
 
 //
 //    Given a new movie has released
@@ -70,5 +66,29 @@ public class MovieControllerTest {
                 .andExpect(jsonPath("$.movieList[0].title", is("The Avengers")));
     }
 
+
+//    Given the GMDB has a movie
+//    When I visit GMDB movies
+//    Then I should see that movie in GMDB movies
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testIfMovieIsPresentThenISeeMovie() throws Exception {
+       //Arrange
+        Movie movie = new Movie();
+        movie.setTitle("The Avengers");
+        movie.setDirector("Joss Whedon");
+        movie.setActors("Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth");
+        movie.setRelease("2012");
+        movie.setDescription("Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.");
+
+        movieRepository.save(movie);
+
+        RequestBuilder requestBuiler = get("/movies")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuiler).andExpect(status().isOk())
+                .andExpect(jsonPath("$.movieList[0].title", is("The Avengers")));
+    }
 
 }
